3a2b97a1d5c70a85ce4ff3724c464fd07600f60d
Alberto Novelle <=>
2020-04-11 22:27:04 +0200

    add chart command that show bar chart with confirmed, deaths and recovered cases per day in the las 30 days.

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 from config.json_rw import config_dict
 from requests import get
 import matplotlib.pyplot as plt
+import numpy as np
 
 import logging
 
@@ -51,20 +52,69 @@ def covid19(update: Update, context: CallbackContext):
             logger.exception(f'covid19 command fail for country: {country}')
 
 
-def make_chart(days_dict, country, type):
+def make_multiple_barchart(country):
+    days = 30
+    width = 0.8
+    x_axis = np.arange(days)
+    factor = (-1, 0, 1)
+    type_to_color = {'confirmed': 'blue', 'deaths': 'red', 'recovered': 'green'}
+    fig, ax = plt.subplots(figsize=(10,5))
+    for idx, type in enumerate(type_to_color):
+        resp = get(f'https://api.covid19api.com/total/dayone/country/{country}/status/{type}')
+        days_dict = resp.json()
+        dates = [day['Date'] for day in days_dict]
+        new_cases_per_day = [day['Cases'] - days_dict[idx - 1]['Cases'] if idx > 0 else day['Cases'] for idx, day in enumerate(days_dict)]
+        y_axis = new_cases_per_day[-days:]
+        ax.bar(x_axis + (factor[idx] * width/3),
+                y_axis,
+                width=width/3,
+                color=type_to_color[type],
+                label=type)
+    labels = [date[5:10] for date in dates[-days:]]
+    ax.set_xticks(x_axis)
+    ax.set_xticklabels(labels, rotation='vertical')
+    ax.set_title(f'{country.capitalize()}: confirmed/deaths/recovered per day in the last {days} days')
+    ax.set_xlabel('Date (mm-dd)')
+    ax.set_ylabel(f'New confirmed/deaths/recovered')
+    ax.legend()
+    fig.tight_layout
+    return plt
+
+
+def chart(update: Update, context: CallbackContext):
+    logger.info('chart command received')
+    countries = context.args if context.args else ['spain']
+    for country in countries:
+        try:
+            resp = get(f'https://api.covid19api.com/total/dayone/country/{country}/status/confirmed')
+            days_dict = resp.json()
+            if not days_dict:
+                raise Exception('Empty json')
+            multi_plt = make_multiple_barchart(country)
+            img_path = r'.\images\img.png'
+            multi_plt.savefig(img_path)
+            context.bot.send_photo(
+                chat_id=update.message.chat_id,
+                photo=open(img_path, 'rb')
+            )
+            plt.close()
+        except:
+            logger.exception(f'covid19 command fail for country: {country}')
+
+
+def make_barchart(days_dict, country, type):
+    days = 30
     dates = [day['Date'] for day in days_dict]
     new_cases_per_day = [day['Cases'] - days_dict[idx - 1]['Cases'] if idx > 0 else day['Cases'] for idx, day in enumerate(days_dict)]
-    x_axis = [idx + 1 for idx, _ in enumerate(new_cases_per_day[-30:])]
-    y_axis = new_cases_per_day[-30:]
+    x_axis = [idx + 1 for idx, _ in enumerate(new_cases_per_day[-days:])]
+    y_axis = new_cases_per_day[-days:]
     plt.bar(x_axis, y_axis)
-    labels = [date[5:10] for date in dates[-30:]]
-    plt.xticks([idx + 1 for idx, _ in enumerate(new_cases_per_day[-30:])], labels, rotation='vertical')
-    plt.title(f'{country.capitalize()}: {type} per day in the last 30 days')
+    labels = [date[5:10] for date in dates[-days:]]
+    plt.xticks([idx + 1 for idx, _ in enumerate(labels)], labels, rotation='vertical')
+    plt.title(f'{country.capitalize()}: {type} per day in the last {days} days')
     plt.xlabel('Date (mm-dd)')
-    plt.ylabel('New cases')
-    img_path = r'.\images\img.png'
-    plt.savefig(img_path)
-    return img_path
+    plt.ylabel(f'New {type}')
+    return plt
 
 
 def chart_confirmed(update: Update, context: CallbackContext):
@@ -76,7 +126,9 @@ def chart_confirmed(update: Update, context: CallbackContext):
             days_dict = resp.json()
             if not days_dict:
                 raise Exception('Empty json')
-            img_path = make_chart(days_dict, country, 'confirmed')
+            plt_confirmed = make_barchart(days_dict, country, 'confirmed')
+            img_path = r'.\images\img.png'
+            plt_confirmed.savefig(img_path)
             context.bot.send_photo(
                 chat_id=update.message.chat_id,
                 photo=open(img_path, 'rb')
@@ -95,7 +147,9 @@ def chart_deaths(update: Update, context: CallbackContext):
             days_dict = resp.json()
             if not days_dict:
                 raise Exception('Empty json')
-            img_path = make_chart(days_dict, country, 'deaths')
+            plt_deaths = make_barchart(days_dict, country, 'deaths')
+            img_path = r'.\images\img.png'
+            plt_deaths.savefig(img_path)
             context.bot.send_photo(
                 chat_id=update.message.chat_id,
                 photo=open(img_path, 'rb')
@@ -114,7 +168,9 @@ def chart_recovered(update: Update, context: CallbackContext):
             days_dict = resp.json()
             if not days_dict:
                 raise Exception('Empty json')
-            img_path = make_chart(days_dict, country, 'recovered')
+            plt_recovered = make_barchart(days_dict, country, 'recovered')
+            img_path = r'.\images\img.png'
+            plt_recovered.savefig(img_path)
             context.bot.send_photo(
                 chat_id=update.message.chat_id,
                 photo=open(img_path, 'rb')
@@ -129,6 +185,7 @@ if __name__ == '__main__':
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('covid19', covid19))
+    dispatcher.add_handler(CommandHandler('chart', chart))
     dispatcher.add_handler(CommandHandler('chartConfirmed', chart_confirmed))
     dispatcher.add_handler(CommandHandler('chartDeaths', chart_deaths))
     dispatcher.add_handler(CommandHandler('chartRecovered', chart_recovered))
